8ed06a0c371bcc597e63c454b5da82f3ec30d1d8
Davide Marchica <123973018+davide122@users.noreply.github.com>
2023-08-08 20:49:29 +0200

    Add files via upload
    linkedinproject

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaBeer } from 'react-icons/fa';
+
+
+import { Row } from'react-bootstrap';
+import LateralComponent from './components/LateralComponent';
+import { Col } from'react-bootstrap';
+import { Container } from'react-bootstrap';
+
+function App() {
+  return (
+ <>
+ <Container>
+ <Row>
+<Col md={8}>
+  lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt lorem ipsum dolor sit amet, consectetur adip
+</Col>
+<Col md={4}>
+<LateralComponent></LateralComponent>
+</Col>
+ </Row>
+ </Container>
+ </>
+  );
+}
+
+export default App;
